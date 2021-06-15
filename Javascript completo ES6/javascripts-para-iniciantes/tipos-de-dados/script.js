dc3c40277c2f43c876df6cd462b95242b2f5f256
Luiz Antônio de Araújo Cipriano <77516067+luizcipriano@users.noreply.github.com>
2021-06-14 23:55:37 -0300

    14/06 inicio do curso de js

---
+++ Javascript completo ES6/javascripts-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,19 @@
+// Declare uma variável contendo uma string
+    var item = "Computador"
+// Declare uma variável contendo um número dentro de uma string
+    var dias = "7"
+// Declare uma variável com a sua idade
+    var idade = 23
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+    var nome = "Luiz",
+    sobrenome = "Cipriano";
+    console.log(nome + sobrenome);
+
+// Coloque a seguinte frase em uma variável: It's time
+    var frase = "It's time"
+// Verifique o tipo da variável que contém o seu nome
+    var nomeCompleto = `${nome} ${sobrenome}`
+
+console.log(nomeCompleto)
+console.log(typeof nomeCompleto)
---
+++ Javascript completo ES6/javascripts-para-iniciantes/variaveis/script.js
@@ -0,0 +1,48 @@
+var nome = 'Luiz';
+var idade = '23'
+var possuiFaculdade = true
+
+
+console.log(nome, idade, possuiFaculdade)
+
+var preco = 25; 
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco)
+
+var sobrenome = "Cipriano",
+    cidade = "errejota";
+
+    console.log(sobrenome, cidade);
+
+
+ // Declarar uma variavel com o meu nome
+var nome = "Luizin";
+
+console.log(nome);
+
+//  Declarar uma variavel com a minha idade 
+
+var idade = "23";
+
+console.log(idade);
+
+//  Declarar uma variavel com a sua comida favorita e não atribuir valor
+
+var comidadeFavorita;
+
+//  Atribuir valor a sua comida favorita
+
+comidadeFavorita = "Frango a parmejiana"
+
+console.log(comidadeFavorita);
+
+
+//  Declarar 5 variáveis diferentes sem valores 
+
+var dia,
+    mes,
+    ano,
+    rua,
+    bairro;
