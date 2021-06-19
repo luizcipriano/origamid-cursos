ae9a1116ae2366025c190660a699b03b89fcbef2
Luiz Antônio de Araújo Cipriano <77516067+luizcipriano@users.noreply.github.com>
2021-06-19 20:00:40 -0300

    19/06

---
+++ Javascript completo ES6/javascripts-para-iniciantes/numeros-e-operadores/script.js
@@ -0,0 +1,36 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+
+// RESPOSTA = O valor é 35 //  
+
+
+// Crie duas expressões que retornem NaN
+
+var first = '40s'
+var second = '50s'
+
+console.log(+first)
+console.log(-second)
+
+
+// Somar a string '200' com o número 50 e retornar 250
+
+var x = '200',
+    y = 50
+console.log(+x + y) 
+
+var soma = +'200' + 50
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+var z = 5; 
+console.log(++z)
+
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = ' kg';
+var peso = numero + unidade; // '80kg'
+
+
+
