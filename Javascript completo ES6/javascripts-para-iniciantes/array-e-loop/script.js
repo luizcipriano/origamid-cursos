7c038c0834aad1e6f08e9cea3dc4847acd9f0df6
Luiz Antônio de Araújo Cipriano <77516067+luizcipriano@users.noreply.github.com>
2021-07-08 16:57:00 -0300

    mais sobre objetos

---
+++ Javascript completo ES6/javascripts-para-iniciantes/array-e-loop/script.js
@@ -0,0 +1,18 @@
+let videoGames = ['Switch', 'PS5', 'XBOX'];
+
+for(let item = 0; item < videoGames.length; item++){
+    console.log(videoGames[item]);
+}
+
+
+
+
+
+
+
+var i = 0;
+while (i<=10){
+    console.log(i);
+    i = i + 5;
+}
+
---
+++ Javascript completo ES6/javascripts-para-iniciantes/tudo-eh-objeto/script.js
@@ -0,0 +1,24 @@
+// nomeie 3 propriedades ou métodos de strings
+
+let nome = 'Luiz';
+// #1-fixed
+// #2-length
+// #3-slice
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+let btn = document.querySelector('.btn');
+
+// 1-addEventListener
+// 2-dispatchEvent
+// 3-webkitMatchesSelector
+// 4-appendChild
+// 5-getAttribute
+
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard
+
+// https://clipboardjs.com/
