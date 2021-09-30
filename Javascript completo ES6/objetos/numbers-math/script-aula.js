3eacfb91301d4b71b50904bfd126ccee2ebff305
Luiz Cipriano <77516067+luizcipriano@users.noreply.github.com>
2021-09-29 22:42:12 -0300

    Number and Math

---
+++ Javascript completo ES6/objetos/numbers-math/script-aula.js
@@ -0,0 +1,65 @@
+console.log(Number.isNaN(NaN)); //true
+Number.isNaN(4+5); //false
+
+Number.isInteger(260); //true
+Number.isInteger(20.6); //false
+
+
+parseFloat('99.50'); // Mesma função sem o Number
+Number.parseFloat('99.50'); // 99.5
+Number.parseFloat('100 Reais'); // 100
+Number.parseFloat('R$ 100'); // NaN
+
+parseInt('99.50', 10); // 99
+parseInt(5.43434355555, 10); // 5
+Number.parseInt('100 Reais', 10); // 100
+
+//N.TOFIXED(DECIMAIS)
+//Arredonda o número com base no total de casas decimais do argumento.
+
+const preco = 2.99;
+preco.toFixed(); // 3
+
+const carro = 1000.455;
+carro.toFixed(2) // 1000.46
+
+const preco2 = 1499.49;
+preco2.toFixed() // 1499
+
+//N.TOSTRING(RADIX)
+//Transforma o número em uma string com base no Radix. Use o 10 para o sistema decimal.
+
+const preco = 2.99;
+preco.toString(10); // '2.99'
+
+//N.TOLOCALESTRING(LANG, OPTIONS);
+//Formata o número de acordo com a língua e opções passadas.
+
+const preco = 59.49;
+preco.toLocaleString('en-US', {style: 'currency', currency: 'USD'}); // $59.49
+preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}); // R$ 59,49
+
+//MATH.ABS(), MATH.CEIL(), MATH.FLOOR() E MATH.ROUND()
+//abs() retorna o valor absoluto, ou seja, transforma negativo em positivo. ceil() arredonda para cima, retornando sempre uma integral e floor para baixo. round() arredonda para o número integral mais próximo.
+
+Math.abs(-5.5); // 5.5
+Math.ceil(4.8334); // 5
+Math.ceil(4.3); // 5
+Math.floor(4.8334); // 4
+Math.floor(4.3); // 4
+Math.round(4.8334); // 5
+Math.round(4.3); // 4
+
+//MATH.MAX(), MATH.MIN() E MATH.RANDOM();
+//max() retorna o maior número de uma lista de argumentos, min() o menor número e random() um número aleatório entre 0 e 1.
+
+Math.max(5,3,10,42,2); // 42
+Math.min(5,3,10,42,2); // 2
+
+Math.random(); // 0.XXX
+Math.floor(Math.random() * 100); // entre 0 e 100
+Math.floor(Math.random() * 500);  // entre 0 e 500
+
+// Número random entre 72 e 32
+Math.floor(Math.random() * (72 - 32 + 1)) + 32; 
+Math.floor(Math.random() * (max - min + 1)) + min;
