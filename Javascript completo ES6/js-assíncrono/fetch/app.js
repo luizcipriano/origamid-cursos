4cc1e364d94d2df1866d6d3540c3855ecce647bd
Luiz Cipriano <77516067+luizcipriano@users.noreply.github.com>
2022-04-26 00:55:15 -0300

    --Usando fetch para buscar um cep, e devolver info

---
+++ Javascript completo ES6/js-assíncrono/fetch/app.js
@@ -0,0 +1,74 @@
+// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+// crie um formulário onde o usuário pode digitar o cep
+// e o endereço completo é retornado ao clicar em buscar
+
+// Utilizando a API https://blockchain.info/ticker
+// retorne no DOM o valor de compra da bitcoin and reais.
+// atualize este valor a cada 30s
+
+// Utilizando a API https://api.chucknorris.io/jokes/random
+// retorne uma piada randomica do chucknorris, toda vez que
+// clicar em próxima
+function buscar(){
+  const cep = document.getElementById('cep').value;
+  console.log(cep);
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  .then(response => response.json())
+  .then(cep => {
+  const bairro = cep.bairro;
+  const rua = cep.logradouro;
+  const cidade = cep.localidade;
+  const uf = cep.uf;
+  const bairro2 = document.createElement('div');
+  const rua2 = document.createElement('div');
+  const cidade2 = document.createElement('div');
+  const uf2 = document.createElement('div');
+
+
+
+    const textBairro = document.createTextNode(`${rua}`);
+    bairro2.appendChild(textBairro);
+    document.querySelector('.bairro').appendChild(bairro2);
+
+
+    const textRua = document.createTextNode(`${rua}`);
+    rua2.appendChild(textRua);
+    document.querySelector('.rua').appendChild(rua2);
+
+    const textCidade = document.createTextNode(`${cidade}`);
+    cidade2.appendChild(textCidade);
+    document.querySelector('.cidade').appendChild(cidade2);
+
+    const textUf = document.createTextNode(`${uf}`);
+    uf2.appendChild(textUf);
+    document.querySelector('.uf').appendChild(uf2);
+
+
+    bairro2.innerHTML = bairro;
+    rua2.innerHTML = rua;
+    cidade2.innerHTML = cidade;
+    uf2.innerHTML = uf;
+
+  // document.body.insertBefore(bairro2, ul),
+  // document.body.insertBefore(rua2, ul),
+  // document.body.insertBefore(cidade2, ul),
+  // document.body.insertBefore(uf2, ul)
+
+  });
+}
+const ul = document.querySelector('ul');
+// function adcElemento () {
+//   // cria um novo elemento div
+//   // e dá à ele conteúdo
+//   var divNova = document.createElement("div");
+//   var conteudoNovo = document.createTextNode("Olá, cumprimentos!");
+//   divNova.appendChild(conteudoNovo); //adiciona o nó de texto à nova div criada
+
+//   // adiciona o novo elemento criado e seu conteúdo ao DOM
+//   var divAtual = document.getElementById("div1");
+//   document.body.insertBefore(divNova, divAtual);
+// }
+
+
+
+
